8066dc9748c1b9acf2264288dd6b722375245958
911samuel <abayizeraeaz@gmail.com>
2024-09-26 08:23:01 +0200

    extracting UI logic

--- ATM.py
+++ ATM.py
@@ -22,59 +22,79 @@ class ATM:
         self.balance -= amount
         return True
     
-def get_number(prompt):
-    while True:
-        try:
-            number = float(input(prompt))
-            return number
-        except ValueError:
-            print("Invalid input. Please enter a number.")
+class ATMController:
+    '''ATM controller class'''
+
+    def __init__(self) -> None:
+        self.atm = ATM()
+
+    def get_number(self,prompt):
+        while True:
+            try:
+                number = float(input(prompt))
+                return number
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+    
+    def display(self):
+        while True:
+            print('\n WELCOME TO ATM MACHINE')
+            print('1. Check Balance')
+            print('2. Deposit')
+            print('3. Withdraw')
+            print('4. Exit')
+
+    def check_balance(self):
+        balance = self.atm.check_balance()
+        print(f'Your current balance is ${balance}')
+
+    def deposit(self):
+        while True:
+            try:
+                amount = self.get_number('Enter the amount to deposit: ')
+                self.atm.deposit(amount)
+                balance = self.atm.check_balance()
+                print(f'Deposited ${amount}. Your new balance is ${balance}')
+                break
+            except ValueError as e:
+                print(f'Error: {e}')
+
+    def withdraw(self):
+        while True:
+            try:
+                amount = self.get_number('Enter the amount to withdraw: ')
+                self.atm.withdraw(amount)
+                balance = self.atm.check_balance()
+                print(f'Withdrew ${amount}. Your new balance is ${balance}')
+                break
+            except ValueError as e:
+                print(f'Error: {e}')
+
+    def run(self):
+        while True:
+            self.display()
+            choice = input('Please choose an option: ')
+
+            if choice == '1':
+                self.check_balance()
+            elif choice == '2':
+                self.deposit()
+            elif choice == '3':
+                self.withdraw()
+            elif choice == '4':
+                print('Goodbye! thank you for using ATM')
+                break
+            else:
+                print('Invalid option. Please choose a valid option.')
+
+
+
 
 
 def main():
-    atm = ATM()
-    while True:
-        print('\n WELCOME TO ATM MACHINE')
-        print('1. Check Balance')
-        print('2. Deposit')
-        print('3. Withdraw')
-        print('4. Exit')
+    atm_controller = ATMController()
+    atm_controller.run()
 
-        choice = input('Please choose an option: ')
-
-        if choice == '1':
-            balance = atm.check_balance()
-            print(f'Your current balance is ${balance}')
-
-        elif choice == '2':
-            while True:
-                try:
-                    amount = get_number('Enter the amount to deposit: ')
-                    atm.deposit(amount)
-                    balance = atm.check_balance()
-                    print(f'Deposited ${amount}. Your new balance is ${balance}')
-                    break
-                except ValueError as e:
-                    print(f'Error: {e}')
-
-        elif choice == '3':
-            while True:
-                try:
-                    amount = get_number('Enter the amount to withdraw: ')
-                    atm.withdraw(amount)
-                    balance = atm.check_balance()
-                    print(f'Withdrew ${amount}. Your new balance is ${balance}')
-                    break
-                except ValueError as e:
-                    print(f'Error: {e}')
-
-        elif choice == '4':
-            print('Goodbye! thank you for using ATM')
-            break
-
-        else:
-            print('Invalid option. Please choose a valid option.')
-
-
+    
 if __name__ == '__main__':
     main()
